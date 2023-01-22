385b19e8e5257581f68f712b4328916aa4b4a59e
Ivan Meshkov <vanek.meshkoff@yandex.ru>
2023-01-22 21:11:22 +0300

    Ex01 Square number added

---
+++ Ex01_SquareNumber/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "");
+int resaultSquare = number * number;
+Console.WriteLine("Квадрат числа " + number + " будет равен " + resaultSquare);
