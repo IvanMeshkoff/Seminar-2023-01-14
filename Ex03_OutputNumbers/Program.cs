67118d4b8b2c11dba0f6e772cb9c0518fb9094d6
Ivan Meshkov <vanek.meshkoff@yandex.ru>
2023-01-22 22:57:41 +0300

    Ex03 Output of numbers added

---
+++ Ex03_OutputNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿// Показать числа от -N до N
+
+Console.Clear();
+
+//Первый способ (через оператор "while"):
+
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+int negN = -N;
+
+while (negN <= N)
+{
+    Console.Write(negN + "  ");
+    negN++;
+}
+Console.WriteLine();
+
+
+//Второй способ (через цикл "for"):
+Console.WriteLine();
+int H = new Random().Next(10);
+Console.WriteLine("Случайное число N: ");
+Console.WriteLine(H);
+
+for (int i = -H; i <= H; i++)
+{
+    Console.Write(i + "  ");
+}
+
